b7b8b97d595381c1776591848293f06312dede7d
Rahil Azhar <rahil.azhar10.com>
2023-09-26 14:11:30 +0500

    second commit

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -4,7 +4,7 @@ import map from '../Assets/images/noun-pakistan-264183 1.png'
 const Map = () => {
     return (
         <section className=' lg:mt-5'>
-            <div className='lgh'>Mission Statement</div>
+            <div className='lgh'>We Build Leaders For Professional Community</div>
             <div className="grid lg:grid-cols-2 md:grid-cols-2 xl:grid-cols-2 lg:px-20 lg:py-10 md:px-3 md:py-10 max-sm:p-1 ">
                 <div className='max-sm:order-1 '> 
                     <div className='bg-blue-500 rounded-xl text-white xl:w-[541px] xl:h-[239px] xl:py-[47px] xl:px-[32px]  lg:w-[362px] lg:h-[200px] lg:px-[13px] lg:py-[26px] md:w-[362px] md:h-[200px] md:px-[10px] md:py-[20px] max-sm:px-3 max-sm:py-10'>
@@ -13,7 +13,7 @@ const Map = () => {
                     </div>
 
                     <div className='bg-red-500 text-white rounded-xl mt-2 xl:py-[47px] xl:px-[32px] xl:w-[541px] xl:h-[311px] lg:w-[362px] lg:h-[250px] lg:px-[13px] lg:py-[26px] md:w-[362px] md:h-[250px] md:px-[10px] md:py-[20px]  max-sm:px-3 max-sm:py-10'>
-                        <div className='text-[30px] font-semibold'> <span className='mr-3'><i class="fa-solid fa-bullseye fa-fade"></i></span>Our Mission</div>
+                        <div className='text-[30px] font-semibold'> <span className='mr-3'><i class="fa-solid fa-bullseye fa-fade"></i></span>Mission Statement</div>
                         <div className='text-[18px] font-normal xl:w-[480px] leading-7 max-sm:text-[16px]'>To design and maintain the quality of knowledge based capacity building programs to empower with new job oriented technical courses and bring positive transformation on their business and professional growth.</div>
                     </div>
                 </div>
