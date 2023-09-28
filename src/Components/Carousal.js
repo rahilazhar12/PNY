5315904ad6a0058d44bc09920393cb61fd0d68b2
Rahil Azhar <rahil.azhar10.com>
2023-09-28 12:50:13 +0500

    second commit

--- src/Components/Carousal.js
+++ src/Components/Carousal.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Carousel } from 'flowbite-react';
-import c1 from '../Assets/image/c1.png'
-import c2 from '../Assets/image/c2.png'
-import c3 from '../Assets/image/c3.png'
+import c1 from '../Assets/Collaboration/1.png'
+import c2 from '../Assets/Collaboration/2.png'
+import c3 from '../Assets/Collaboration/3.png'
+import c4 from '../Assets/Collaboration/4.png'
+import c5 from '../Assets/Collaboration/5.png'
+import c6 from '../Assets/Collaboration/6.png'
+import c7 from '../Assets/Collaboration/7.png'
 
 import g1 from '../Assets/image/g1.png'
 import g2 from '../Assets/image/g2.png'
@@ -50,6 +54,22 @@ export const Carousalhome = () => {
       <img
         alt="..."
         src={c3}
+      />
+      <img
+        alt="..."
+        src={c4}
+      />
+      <img
+        alt="..."
+        src={c5}
+      />
+      <img
+        alt="..."
+        src={c6}
+      />
+      <img
+        alt="..."
+        src={c7}
       />
     </Carousel>
     </div>
