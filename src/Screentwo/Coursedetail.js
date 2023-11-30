651b071cb968861d62fc593c993500ce1e73ff55
Rahil Azhar <rahil.azhar10.com>
2023-11-30 13:49:02 +0500

    first commit

--- src/Screentwo/Coursedetail.js
+++ src/Screentwo/Coursedetail.js
@@ -107,23 +107,23 @@ const Coursedetail = () => {
     };
 
     fetchCourseData();
-    // window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
   }, [courseSlug]);
 
-  // if (isLoading) {
-  //   return (
-  //     <div className="flex justify-center items-center min-h-screen">
-  //       <Blocks
-  //         visible={true}
-  //         height="80"
-  //         width="80"
-  //         ariaLabel="blocks-loading"
-  //         wrapperStyle={{}}
-  //         wrapperClass="blocks-wrapper"
-  //       />
-  //     </div>
-  //   );
-  // }
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <Blocks
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="blocks-loading"
+          wrapperStyle={{}}
+          wrapperClass="blocks-wrapper"
+        />
+      </div>
+    );
+  }
 
   const parsedDescription = parse(courseData.description, {
     replace: domNode => {
